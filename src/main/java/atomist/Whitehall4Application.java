b57363aa3e2356a35ccbe5857f4f8a5a630b1107
Rod Johnson <johnsonroda@gmail.com>
2018-03-05 00:05:39 +0000

    Initial commit from Atomist

---
+++ src/main/java/atomist/Whitehall4Application.java
@@ -0,0 +1,12 @@
+package atomist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Whitehall4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Whitehall4Application.class, args);
+	}
+}
